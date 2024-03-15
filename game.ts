ab936cca8ea90bb5648323382932e94a21be939e
JohnySilverconor <60777159+JohnySilverconor@users.noreply.github.com>
2024-03-15 02:19:42 -0300

    Add files via upload

--- game.ts
+++ game.ts
@@ -1,21 +1,30 @@
-//configuration for your game: width,height, defining physics
 import 'phaser'
+import MainScene from './scenes/mainScene'
+import PreloadScene from './scenes/preloadScene'
 
 const DEFAULT_WIDTH = 1280
 const DEFAULT_HEIGHT = 720
 
 const config = {
-
-    type: Phaser.AUTO,
-    backgroundColor:"#ffffe0" ,
-    scale:{
-parent: 'phaser-game',
-mode:Phaser.Scale.FIT,
-autoCenter: Phaser.Scale.CENTER_BOTH,
-width:DEFAULT_WIDTH,
-height: DEFAULT_HEIGHT
+  type: Phaser.AUTO,
+  backgroundColor: '#ffffe0',
+  scale: {
+    parent: 'phaser-game',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT
+  },
+  scene: [PreloadScene, MainScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: 400 }
     }
+  }
 }
-window.addEventListener('load', ()=>{
-    const game = new Phaser.Game(config)
-})
+
+
+export default new Phaser.Game(config)
+
